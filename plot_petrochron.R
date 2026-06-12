4674f844b4820746368b03ebe35ceba9d2552010
Lucas Ramos Tesser <lucas.tesser@unil.ch>
2026-06-12 14:29:50 -0300

    Update monazite plot script with garnet data

--- plot_petrochron.R
+++ plot_petrochron.R
@@ -1,5 +1,10 @@
+#------------------------------------------------------------------------------#
+#-------------- SCRIPT FOR PLOTING MONAZITE TRACE ELEMENTS  -------------------#
+#--------------------- By Lucas R. Tesser, USP - Brazil -----------------------#
+#------------------------------------------------------------------------------#
+
 #------------------------ Uploading the libraries-------------------------------
-library(openxlsx)
+library(openxlsx) 
 library(ggplot2)
 library(GGally)
 library(viridis)
@@ -11,60 +16,80 @@ data <- read.xlsx(file.choose()) # import the data from excel
 data_org <- data[order(data$Age),] # organize the dataframe by age
 
 #----------------------- Input chondrite composition --------------------------#
-# Create a data frame with CI chondrite composition
-chon <- data.frame('Reference' = c('CI_McDonough_Sun_1995'), 
-                   'La' = c(0.237), 'Ce' = c(0.613), 'Pr' = c(0.0928), 
-                   'Nd' = c(0.457), 'Sm' = c(0.148), 'Eu' =c(0.0563), 
-                   'Gd' = c(0.199), 'Tb' = c(0.0361), 'Dy' = c(0.246), 
-                   'Ho' = c(0.0546), 'Er' = c(0.16), 'Tm' = c(0.0247), 
-                   'Yb' = c(0.161), stringsAsFactors = FALSE)
+# Create a data frame with garnet composition
+chon <- data.frame('Reference' = c('Grt1'), 
+                   'La' = c(0.2939), 'Ce' = c(0.6046), 'Pr' = c(0.1369), 
+                   'Nd' = c(0.8715), 'Sm' = c(1.0030), 'Eu' =c(0.4460), 
+                   'Gd' = c(5.2540), 'Tb' = c(1.8000), 'Dy' = c(13.3900), 
+                   'Ho' = c(2.9180), 'Er' = c(8.5830), 'Tm' = c(1.5190), 
+                   'Yb' = c(11.9800), stringsAsFactors = FALSE)
+
+# chon <- data.frame('Reference' = c('Grt2'), 
+#                    'La' = c(0.2491), 'Ce' = c(0.5952), 'Pr' = c(0.1451), 
+#                    'Nd' = c(0.8544), 'Sm' = c(1.4110), 'Eu' =c(0.5420), 
+#                    'Gd' = c(12.6200), 'Tb' = c(4.5450), 'Dy' = c(13.3900), 
+#                    'Ho' = c(4.6040), 'Er' = c(9.6290), 'Tm' = c(1.1630), 
+#                    'Yb' = c(7.8050), stringsAsFactors = FALSE)
+
+# chon <- data.frame('Reference' = c('Grt3'), 
+#                    'La' = c(0.2067), 'Ce' = c(0.1992), 'Pr' = c(0.0650), 
+#                    'Nd' = c(0.4085), 'Sm' = c(1.1380), 'Eu' =c(0.3861), 
+#                    'Gd' = c(8.5940), 'Tb' = c(1.5120), 'Dy' = c(6.9650), 
+#                    'Ho' = c(0.9535), 'Er' = c(1.9540), 'Tm' = c(0.2725), 
+#                    'Yb' = c(1.3140), stringsAsFactors = FALSE)
+
+equil <- data.frame('Reference' = c('Rubatto2006-SGL5', 'Hacker2019-E9819D1'), 
+                   'La' = c(12172786, 25148575), 'Ce' = c(3207594, 17011927), 
+                   'Pr' = c(567768, 3244703), 'Nd' = c(112660,406824),
+                   'Sm' = c(5936, 10756), 'Eu' =c(1283, 1356), 
+                   'Gd' = c(644, 1456), 'Tb' = c(163, 638), 'Dy' = c(56, 326), 
+                   'Ho' = c(24, 161), 'Er' = c(12, 81), 'Tm' = c(6.5, 48), 
+                   'Yb' = c(3.8, 28), stringsAsFactors = FALSE)
 
 # Create a new dataframe to store the normalized values
 normalized_data <- data_org[, c(2, 5, 6:18)]
 
 for (i in 1:dim(data_org)[1]) {
-  normalized_data[i, 3:14] <- data_org[i, 6:18] / chon[1, 2:14]
+  normalized_data[i, 3:15] <- data_org[i, 6:18] / chon[1, 2:14]
 }
 
 normalized_equil <- equil[, c(2, 3:14)]
 
 # First plot
 plot1 <- ggparcoord(normalized_data, 
-                    columns = 3:15, # Range of data to be plotted
+                    columns = 9:15, # Range of data to be plotted
                     groupColumn = 1, # 1 = Age
                     showPoints = FALSE, # disable/enable points in the lines
                     scale="globalminmax", 
-                    title = "My sample", # graph title
+                    title = "DLT147", # graph title
                     boxplot = FALSE) +
   scale_y_log10(oob = scales::squish_infinite, 
-                limits = c(1, 1000000)) + # define limits of y-axis
-  scale_color_viridis(option = "A", # select A to H to change the color scheme
+                limits = c(1.0, 10000)) + # define limits of y-axis
+  scale_color_viridis(option = "D", # select A to H to change the color scheme
                       discrete = FALSE, 
-                      direction = 1) +
+                      direction = -1) +
   theme_light()+
-  labs(x = "Trace Element", y = "Monazite/Chondrite") +
+  labs(x = "Trace Element", y = "Monazite/Garnet") +
   theme(panel.grid.major = element_blank(),
         panel.grid.minor = element_blank()) +
-  geom_line(linewidth = 1, alpha = 0.3) # Change line thickness and alpha
+  geom_line(linewidth = 1.2, alpha = 0.5) # Change line thickness and alpha
 
-# Second plot
-plot2 <- ggparcoord(normalized_data, 
-                    columns = 3:15, # Range of data to be plotted
-                    groupColumn = 2, # 2 = Y in ppm
-                    showPoints = FALSE, # disable/enable points in the lines
+# Second plot (equilibrium monazite-garnet)
+ plot2 <- ggparcoord(normalized_equil, 
+                    columns = 7:13, # Range of data to be plotted
+                    showPoints = TRUE, # disable/enable points in the lines
                     scale="globalminmax", 
-                    title = "My sample", # graph title
+                    title = "Monazite–garnet REE equilibrium",
                     boxplot = FALSE) +
   scale_y_log10(oob = scales::squish_infinite, 
-                limits = c(1, 1000000)) + # define limits of y-axis
-  scale_color_viridis(option = "A", # select A to H to change the color scheme
-                      discrete = FALSE, 
-                      direction = 1) +
+                limits = c(1.0, 10000)) + # define limits of y-axis
   theme_light() +
-  labs(x = "Trace Element", y = "Monazite/Chondrite") +
+  labs(x = "Trace Element", y = "Monazite-Garnet") +
   theme(panel.grid.major = element_blank(),
         panel.grid.minor = element_blank()) +
-  geom_line(linewidth = 1, alpha = 0.3) # Change line thickness and alpha
-
+  geom_line(linewidth = 0.6, alpha = 0.4) # Change line thickness and alpha
 # Arrange the plots side by side
-grid.arrange(plot1, plot2, ncol = 2)
+grid.arrange(plot1, 
+             plot2, 
+             ncol = 2)
+# --------------------------------- END --------------------------------------#
